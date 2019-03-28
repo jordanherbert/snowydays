9f941b8a1c6e5dbdf2aa8b78d8e76417b04cf38b
Jordan Herbert <jbherb17@gmail.com>
2019-03-27 22:21:16 -0700

    first commit

---
+++ functions.js
@@ -0,0 +1,40 @@
+let can1,p1,can2,p2;
+
+function yes(){
+  can1=createCanvas(windowWidth,windowHeight-30);
+  can1.position(0,0);
+  can1.background(255,255,0);
+  textAlign(CENTER,CENTER);
+ textSize(50);
+ text('Great choice, come outside!',width/2,height/2);
+}
+
+function no(){
+   can1=createCanvas(windowWidth,windowHeight-30);
+  can1.position(0,0);
+  can1.background(255,255,0);
+  textAlign(CENTER,CENTER);
+ textSize(40);
+ text("I'm only putting this here to give you the choice. Please say yes.",width/2,height/2);
+}
+
+let radio2;
+function burrito2(){
+radio2 = createRadio();
+radio2.position(windowWidth/2,2*windowHeight/3);
+  radio2.class("bdd");
+  radio2.option('Yes',4);
+  radio2.option('No ',5);
+  radio2.style('width','50px');
+
+}
+
+function drawburrito2(){
+var val2 = radio2.value();
+  if(val2){
+  switch(val2){
+case '4': yes();break; //yes
+case '5': no();break; //no
+	}
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,24 @@
+let can;
+let p;
+function setup() {
+
+can=createCanvas(windowWidth-100,windowHeight-100);;
+can.position(50,50);
+can.class("bdd");
+
+background(255,255,0,180);
+textAlign(CENTER,CENTER);
+textSize(70);
+text('Jolie, will you BEE my date to Mormal?',width/2,height/2);
+
+
+burrito2();
+
+}
+
+
+function draw() {
+//background(125);
+drawburrito2();
+}
+
